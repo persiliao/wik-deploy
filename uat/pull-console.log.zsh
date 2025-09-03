3480b1ec80fdb8a23b6aca0aeda2470f662e16ef
persiliao <xiangchu.liao@gmail.com>
2025-09-03 16:54:25 +0800

    refactor: Optimize Code

--- uat/pull-console.log.zsh
+++ uat/pull-console.log.zsh
@@ -2,8 +2,3 @@
 source ~/.zshrc
 
 copy_remote_logs "WIK.VPN.Dev" "/opt/prod/icp/logs/sys-console.log" "$(dirname $(realpath $0))/logs"
-
-yesterday=$(date -v-1d +%Y-%m-%d)
-
-copy_remote_logs "WIK.VPN.Dev" "/opt/prod/icp/logs/sys-console.$yesterday.log" "$(dirname $(realpath $0))/logs"
-
