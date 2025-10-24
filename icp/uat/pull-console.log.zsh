0da30be22185931146a87bb5638102bd0cf0d883
persiliao <xiangchu.liao@gmail.com>
2025-10-24 14:22:52 +0800

    refactor: Optimize Code

--- icp/uat/pull-console.log.zsh
+++ icp/uat/pull-console.log.zsh
@@ -2,3 +2,5 @@
 source ~/.zshrc
 
 copy_remote_logs "WIK.VPN.Dev" "/opt/prod/icp/logs/sys-console.log" "$(dirname $(realpath $0))/logs"
+
+ssh WIK.VPN.Dev "sudo truncate -s 0 /opt/prod/icp/logs/sys-console.log"
